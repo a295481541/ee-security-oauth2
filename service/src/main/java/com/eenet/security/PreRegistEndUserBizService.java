20583b3d7f646a452f3ecd734ac779779c03845a
Orion <linhao@eenet.com>
2017-01-10 11:13:34 +0800

    调整账号存在检查方式

--- service/src/main/java/com/eenet/security/PreRegistEndUserBizService.java
+++ service/src/main/java/com/eenet/security/PreRegistEndUserBizService.java
@@ -1,7 +1,6 @@
 package com.eenet.security;
 
 import com.eenet.base.BooleanResponse;
-import com.eenet.baseinfo.user.EndUserInfo;
 
 /**
  * 注册新用户服务预先检查服务
@@ -9,25 +8,14 @@ import com.eenet.baseinfo.user.EndUserInfo;
  * @author Orion
  */
 public interface PreRegistEndUserBizService {
-	/**
-	 * 新增最终用户前检查，检查是否存在相同的手机、电子邮箱或身份证号
-	 * @param mobile 手机
-	 * @param email 邮箱
-	 * @param idCard 身份证
-	 * @return true表示存在
-	 * 2016年6月16日
-	 * @author Orion
-	 */
-	public BooleanResponse existMobileEmailId(String mobile, String email, String idCard);
 	
 	/**
-	 * 根据手机、电子邮箱或身份证号获得最终用户个人信息
-	 * @param mobile 手机
-	 * @param email 邮箱
-	 * @param idCard 身份证
-	 * @return 如果找个一条符合记录的数据则返回，其他情况均返回错误信息
-	 * 2016年6月22日
+	 * 账号是否可注册
+	 * @param SeriesOrAppId 业务体系或业务系统ID，如果是无业务体系的应用ID则会被拒绝
+	 * @param account 待注册账号
+	 * @return result属性为true表示可注册
+	 * 2017年1月10日
 	 * @author Orion
 	 */
-	public EndUserInfo getByMobileEmailId(String mobile, String email, String idCard);
+	public BooleanResponse existAccount(String SeriesOrAppId, String[] accounts);
 }
