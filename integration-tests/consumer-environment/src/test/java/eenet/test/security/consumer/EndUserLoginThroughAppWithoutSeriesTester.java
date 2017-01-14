4716c27e66e0ef6b60223787cc2f1e64d14d5d2c
Orion <linhao@eenet.com>
2017-01-14 17:50:50 +0800

    定义测试类

---
+++ integration-tests/consumer-environment/src/test/java/eenet/test/security/consumer/EndUserLoginThroughAppWithoutSeriesTester.java
@@ -0,0 +1,14 @@
+package eenet.test.security.consumer;
+
+/**
+ * 单元测试：最终用户通过有体系系统登录
+ * 2017年1月14日
+ * @author Orion
+ */
+public class EndUserLoginThroughAppWithoutSeriesTester {
+
+	public EndUserLoginThroughAppWithoutSeriesTester() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ integration-tests/consumer-environment/src/test/java/eenet/test/security/consumer/env/SpringEnvironment.java
@@ -0,0 +1,9 @@
+package eenet.test.security.consumer.env;
+
+public class SpringEnvironment {
+
+	public SpringEnvironment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
